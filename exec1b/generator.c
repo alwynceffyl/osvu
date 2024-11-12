22bbafdafa4e54cb2b1a1ba46bcd1f9d4147a6b0
Phillip Sassmann <y12207461@apps1.inflab.tuwien.ac.at>
2024-11-12 13:48:23 +0100

    exec1b finished 20/20, and exec2 started

--- exec1b/generator.c
+++ exec1b/generator.c
@@ -1,28 +1,55 @@
 #include "common.h"
 
+
+/**
+ * @file generator.c
+ * @author Phillip Sassmann
+ * @date 12.11.2024
+ *
+ * @brief This file contains functions for handling signals, processing
+ *        command-line arguments, setting up shared memory, and managing
+ *        semaphores to implement a simple graph coloring program.
+ */
+
 char* myprog;
 volatile sig_atomic_t quit = 0;
 
+
+
+/**
+ * @details Signal handler to set a flag to terminate the main loop.
+ * 
+ * @param signal The signal number (e.g., SIGINT).
+ */
 void handle_signal(int signal) {
     quit = 1;
 }
 
+
+/**
+ * @details Parses command-line arguments, sets up shared memory and semaphores, 
+ *        and runs the main processing loop for graph coloring until a termination
+ *        signal is received or the shared buffer indicates a stop.
+ * 
+ * @param argc Argument count.
+ * @param argv Argument values.
+ * @return int Exit status (EXIT_SUCCESS or EXIT_FAILURE).
+ */
 int main(int argc, char *argv[]) {
     
     myprog=argv[0];
+    srand((unsigned int)time(NULL));
 
 
     if(argc < 2) usage("we need edges for the graph");
 
     edges_t params[argc-1];
-    
-    for(int i=1; i<argc; i++){
+    int i=1;
+    for(; i<argc; i++){
         char* token = strtok(argv[i], "-");
         params[i-1].node_from.value= strtol(token, NULL , 0) > INT_MAX ? INT_MAX : strtol(token, NULL , 0) ;
-        params[i-1].node_from.colour=-1;
         token = strtok(NULL, "-");
         params[i-1].node_to.value= strtol(token, NULL , 0) > INT_MAX ? INT_MAX : strtol(token, NULL , 0) ;
-        params[i-1].node_to.colour=-1;
         if(strtok(NULL, "-")!=NULL) usage("too many connected nodes");
         if(errno==ERANGE) usage("error in converting the optarg of [n]");
     } 
@@ -41,6 +68,8 @@ int main(int argc, char *argv[]) {
         perror("error in mapping memory");
         exit(EXIT_FAILURE);
     }
+    circularbuffer->numGen = circularbuffer->numGen +1;
+
 
     struct sigaction sa = { .sa_handler = handle_signal };
     if(sigaction(SIGINT, &sa, NULL)==-1 || sigaction(SIGTERM, &sa, NULL)==-1){  
@@ -60,45 +89,39 @@ int main(int argc, char *argv[]) {
 
     
 
-    circularbuffer->write_pos =0;
-
     while(!quit && !(circularbuffer->stop)){
 
-        if(sem_wait(sem_free)==-1 && errno !=EINTR){
-            perror("error in semaphore wating (generator)");
-            exit(EXIT_FAILURE);
-            break;
-        }
-       
 
         if(sem_wait(sem_generator)==-1 && errno != EINTR){
             perror("error in semaphore wating (generator)");
             exit(EXIT_FAILURE);
             break;
         }
-
-        //Write solution from buffer
-
+        
         edgelist_t solution=colouring(params, (argc-1));
-        if(solution.size < MAX_EDGES){
-            circularbuffer->solution[circularbuffer->write_pos]=colouring(params, (argc-1));
-            circularbuffer->write_pos=(circularbuffer->write_pos+1) % sizeof(circularbuffer);
-        }
-        else{
-            continue;
-        }
 
+        if(solution.size<MAX_EDGES){
+            if(sem_wait(sem_free)==-1 && errno !=EINTR){
+                perror("error in semaphore wating (generator)");
+                exit(EXIT_FAILURE);
+                break;
+            }
+        
+            circularbuffer->solution[circularbuffer->write_pos]=solution;
+            circularbuffer->write_pos=(circularbuffer->write_pos+1) % (LEN);
+            
+
+            if(sem_post(sem_used)==-1){
+                perror("error in semaphore posting (generator)");
+                exit(EXIT_FAILURE);
+                break;
+            }
+        }
         if(sem_post(sem_generator)==-1){
             perror("error in semaphore posting (generator)");
             exit(EXIT_FAILURE);
             break;
-        }        
-
-        if(sem_post(sem_used)==-1){
-            perror("error in semaphore posting (generator)");
-            exit(EXIT_FAILURE);
-            break;
-        }
+        }      
     }
     
     /* CLEAN UP */
@@ -121,26 +144,43 @@ int main(int argc, char *argv[]) {
     exit(EXIT_SUCCESS);
 }
 
-
+/**
+ * @brief Prints an error message and exits the program.
+ *
+ * @details This function is used to display program usage or error messages,
+ * along with the proper command-line syntax.
+ *
+ * @param errormsg Custom error message to display.
+ */
 void usage(char* errormsg) {
     fprintf(stderr, "Usage: %s , errormessage: %s\n",myprog, errormsg);
     exit(EXIT_FAILURE);
 }
 
+/**
+ * @brief Generates a 3-color solution for the graph based on input edges.
+ *
+ * @details The function initializes each node’s color, then attempts to color each node
+ * such that adjacent nodes have different colors. It returns an `edgelist_t` containing
+ * edges that fail to meet the 3-coloring criteria.
+ *
+ * @param params Array of edges representing the graph.
+ * @param size Number of edges in the `params` array.
+ * @return edgelist_t List of edges that do not meet the 3-coloring condition.
+ */
 edgelist_t colouring(edges_t params[], int size){
 
-    srand((unsigned int)time(NULL));
-
-    for(int i=0; i<size; i++){
+    int i=0;
+    for(; i<size; i++){
         params[i].node_from.colour=-1;
         params[i].node_to.colour=-1;
     } 
 
-
-    for(int i=0; i< size; i++){
+    i=0;
+    for(; i< size; i++){
         if(params[i].node_from.colour==-1 && params[i].node_to.colour==-1){
             params[i].node_from.colour=rand() % 3;
-            params[i].node_to.colour=rand() % 3;
+            params[i].node_to.colour= rand() % 3;
         }   
         if(params[i].node_from.colour==-1){
             params[i].node_from.colour = rand() % 3;
@@ -149,7 +189,8 @@ edgelist_t colouring(edges_t params[], int size){
             params[i].node_to.colour =  rand() % 3;
         } 
 
-        for(int j=i+1; j<size; j++){
+        int j=i+1;
+        for(; j<size; j++){
             if(params[i].node_from.value==params[j].node_from.value){
                 params[j].node_from.colour=params[i].node_from.colour;
             }
@@ -167,7 +208,8 @@ edgelist_t colouring(edges_t params[], int size){
  
 
     int counter=0;
-    for(int i=0; i < size; i++){
+    i=0;
+    for(; i < size; i++){
         if(params[i].node_from.colour==params[i].node_to.colour){
             counter++;
         }
@@ -178,7 +220,8 @@ edgelist_t colouring(edges_t params[], int size){
     writeToBuffer.size=counter;
 
     counter=0;
-    for(int i=0; i < size && counter <= MAX_EDGES; i++){
+    i=0;
+    for(; i < size && counter < MAX_EDGES; i++){
         if(params[i].node_from.colour==params[i].node_to.colour){
             writeToBuffer.list[counter].node_from=params[i].node_from;
             writeToBuffer.list[counter].node_to= params[i].node_to;
--- exec1b/supervisor.c
+++ exec1b/supervisor.c
@@ -1,15 +1,43 @@
 #include "common.h"
 
 
+/**
+ * @file supervisor.c
+ * @author Phillip Sassmann
+ * @date 12.11.2024
+ *
+ * @brief This file implements a supervisor process that monitors solutions
+ *        from a shared memory buffer, processes the best solution, and terminates
+ *        when a 3-colorable solution is found or a solution limit is reached.
+ */
+
 char* myprog;
-
-
 volatile sig_atomic_t quit = 0;
 
+
+/**
+ * @brief Signal handler for program termination.
+ * 
+ * @details Sets the global `quit` flag to true, allowing for a graceful exit
+ * from ongoing processes.
+ *
+ * @param signal Signal number, such as `SIGINT` or `SIGTERM`.
+ */
 void handle_signal(int signal) {
     quit = 1;
 }
 
+
+/**
+ * @brief Parses command-line options, initializes shared memory and semaphores, 
+ *        and runs the main supervisor loop to read solutions from the circular 
+ *        buffer. If a 3-colorable solution is found or the solution limit is reached, 
+ *        it exits gracefully.
+ * 
+ * @param argc Argument count.
+ * @param argv Argument values.
+ * @return int Exit status (EXIT_SUCCESS or EXIT_FAILURE).
+ */
 int main(int argc,char *argv[]){
 
     myprog=argv[0];
@@ -50,7 +78,7 @@ int main(int argc,char *argv[]){
         exit(EXIT_FAILURE);
     } 
 
-    if(ftruncate(shmfd, sizeof(edgelist_t))==-1){
+    if(ftruncate(shmfd, sizeof(circularbuffer_t))==-1){
         perror("error in sizing memory ");
         exit(EXIT_FAILURE);
     }
@@ -73,19 +101,7 @@ int main(int argc,char *argv[]){
     sem_t *sem_free = sem_open(SEM_FREE, O_CREAT | O_EXCL , PERMISSIONS, LEN);
     sem_t *sem_used = sem_open(SEM_USED, O_CREAT | O_EXCL , PERMISSIONS, 0);
     sem_t *sem_generator = sem_open(SEM_GENERATOR, O_CREAT | O_EXCL, PERMISSIONS, 1);
-
-
-    /*//in case of bus error
-    if(sem_unlink(SEM_FREE)==-1 || sem_unlink(SEM_USED)==-1 || sem_unlink(SEM_GENERATOR)==-1){
-        perror("error in unlinking semaphores");
-        exit(EXIT_FAILURE);
-    }
-
-    exit(EXIT_FAILURE);*/
-    
-    
-    
-
+        
     if (sem_free == SEM_FAILED || sem_used == SEM_FAILED || sem_generator == SEM_FAILED) {
         perror("error in opening semaphores");
         exit(EXIT_FAILURE);
@@ -93,6 +109,8 @@ int main(int argc,char *argv[]){
 
     circularbuffer->stop =false;
     circularbuffer->read_pos =0;
+    circularbuffer->write_pos =0;
+    circularbuffer->numGen =0;
 
     sleep(delay);
 
@@ -107,21 +125,18 @@ int main(int argc,char *argv[]){
         //Read solution from buffer
         edgelist_t solution=circularbuffer->solution[circularbuffer->read_pos];
         circularbuffer->read_pos=(circularbuffer->read_pos+1) % (LEN);
-        
-        fprintf(stdout, "my size:=%s", solution.size);
-        if(num_solutions==0) bestRemovedEdges=solution.size;
 
         if(solution.size==0){
             bestRemovedEdges=0;
             fprintf(stdout,"The graph is 3-colorable!\n");
             quit=1;
+            circularbuffer->stop;
         }
-
 
         if(solution.size < bestRemovedEdges){
             bestRemovedEdges=solution.size;
-            fprintf(stdout,"Solution with %d edges: %s\n",bestRemovedEdges);
-            printEdges(solution);
+            //printf("Solution with %d edges: ",bestRemovedEdges);
+            //printEdges(solution);
         }
 
         num_solutions++;
@@ -135,10 +150,13 @@ int main(int argc,char *argv[]){
     
 
     if(bestRemovedEdges>0){
-       fprintf(stdout,"The graph might not be 3-colorable,\nbest solution removes %d edges.\n",bestRemovedEdges);
+       fprintf(stdout,"The graph might not be 3-colorable, best solution removes %d edges.\n",bestRemovedEdges);
     }
 
-    circularbuffer->stop=true;
+    while(circularbuffer->numGen>0){
+        sem_post(sem_free);
+        circularbuffer->numGen=circularbuffer->numGen -1 ;
+    }
 
 
     /* CLEAN UP */
@@ -170,15 +188,33 @@ int main(int argc,char *argv[]){
 }
 
 
+/**
+ * @brief Prints an error message and exits the program.
+ *
+ * @details This function is used to display program usage or error messages,
+ * along with the proper command-line syntax.
+ *
+ * @param errormsg Custom error message to display.
+ */
 void usage(char* errormsg) {
     fprintf(stderr, "Usage: %s, supervisor [-n limit] [-w delay], errormessage: %s\n",myprog, errormsg);
     exit(EXIT_FAILURE);
 }
 
+
+/**
+ * @brief Prints the edges in the provided `edgelist_t` solution.
+ *
+ * @details Iterates through the list of edges in `solution` and prints each edge as
+ * a connection between two nodes. Used to display a generated graph solution.
+ *
+ * @param solution List of edges representing a graph solution.
+ */
 void printEdges(edgelist_t solution){
-    for(int i =0; i<solution.size; i++){
-        fprintf(stdout,"%ld-%ld ", solution.list[i].node_from.value,solution.list[i].node_to.value);              
-    }                                                 
+   int i=0;
+    for( ; i<solution.size; i++){
+        printf("%d-%d ", solution.list[i].node_from.value,solution.list[i].node_to.value);              
+    }                                                
   
     printf("\n");
 }
